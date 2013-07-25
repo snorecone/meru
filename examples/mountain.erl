ab56ea5ddcf22fc5a87499c85b6a3c41b36b601a
Mark Martin <assplecake@gmail.com>
2013-07-25 14:53:37 -0700

    partial get implemented

---
+++ examples/mountain.erl
@@ -0,0 +1,83 @@
+-module(mountain).
+-compile({parse_transform, meru_transform}).
+
+-meru_bucket(<<"mountains">>).
+-meru_record(mountain).
+-meru_keyfun(make_key).
+-meru_mergefun(merge).
+
+-export([
+    test/0
+]).
+
+-record(mountain, {
+    name,
+    range,
+    planet,
+    lakes = [],
+    height,
+    type,
+    created_at,
+    updated_at
+}).
+
+test() ->
+    % construct 2 mountain records
+    Chimbo = #mountain{
+        name   = <<"Chimborazo">>,
+        range  = <<"Cordillera Occidental">>,
+        planet = <<"Earth">>,
+        height = 6267,
+        type   = <<"volcano">>
+    },
+    Oly = #mountain{
+        name   = <<"Olympus Mons">>,
+        range  = <<"Amazonis">>,
+        planet = <<"Mars">>,
+        height = 21171,
+        type   = <<"volcano">>
+    },
+    
+    % put our mountains in the store
+    {ok, ChimboKey} = mountain:put(Chimbo),
+    {ok, OlyKey}    = mountain:put(Oly),
+    
+    % update chimborazo
+    ChimboUpdate = #mountain{
+        lakes = [<<"Rio Chambo Dam">>]
+    },
+    {ok, ChimboKey} = mountain:put_merge(ChimboKey, ChimboUpdate, [{lake_merge, union}]),
+    
+    % get the mountains out by key or tuple
+    {ok, Chimbo2} = mountain:get(ChimboKey),
+    {ok, Chimbo2} = mountain:get({<<"Chimborazo">>, <<"Cordillera Occidental">>}),
+    {ok, Oly} = mountain:get(OlyKey),
+    {ok, Oly} = mountain:get({<<"Olympus Mons">>, <<"Amazonis">>}),
+    
+    % deleting a deleted record should return not found
+    {ok, ChimboKey}   = mountain:delete({<<"Chimborazo">>, <<"Cordillera Occidental">>}),
+    {error, notfound} = mountain:delete(ChimboKey),
+    {ok, OlyKey} = mountain:delete(OlyKey).
+
+%%
+%% private
+%%
+
+make_key(#mountain{ name = Name, range = Range }) ->
+    make_key({Name, Range});
+make_key({Name, Range}) -> binary_to_term({Name, Range});
+make_key(Key) when is_binary(Key) -> Key.
+
+% merge(OldMountain, NewMountain, MergeOpts) ->
+%     Lakes = case proplists:get_value(lake_merge) of
+%         overwrite -> 
+%             NewMountain#mountain.lakes;
+%         union -> 
+%             lists:usort(OldMountain#mountain.lakes ++ NewMountain#mountain.lakes)
+%     end,
+%     OldMountain#mountain{
+%         lakes = Lakes,
+%         updated_at = calendar:universal_time()
+%     }.
+% 
+    
--- src/meru_transform.erl
+++ src/meru_transform.erl
@@ -8,46 +8,80 @@
     list_elements/1,
     atom_value/1,
     tuple_elements/1,
-    integer_value/1
+    integer_value/1,
+    binary_fields/1,
+    binary_field_body/1
 ]).
 
 -record(state, {
     record,
-    funs = ordsets:new()
+    bucket,
+    keyfun,
+    mergefun
 }).
 
 parse_transform(Forms, Opts) ->
-    dbg:tracer(),
-    dbg:tpl(?MODULE,x),
-    dbg:p(all,[c]),
+    % dbg:tracer(),
+    % dbg:tpl(?MODULE,x),
+    % dbg:p(all,[c]),
     Context = parse_trans:initial_context(Forms, Opts),
     State = parse_trans:do_inspect(fun inspect/4, #state{}, Forms, Context),
-    Result = parse_trans:revert(export_funs(add_get_function(Forms, State, Context), State, Context)),
+    Result = parse_trans:revert(export_funs(add_funs(Forms, State, Context), State, Context)),
     parse_trans:optionally_pretty_print(Result, Opts, Context),
     Result.
 
 inspect(attribute, Form, _Context, Acc) ->
     case atom_value(attribute_name(Form)) of
+        meru_bucket ->
+            undefined = Acc#state.bucket,
+            BucketName = parse_trans:revert_form(hd(attribute_arguments(Form))),
+            io:format("~p~n", [BucketName]),
+            {false, Acc#state{ bucket = BucketName }};
+        meru_keyfun ->
+            undefined = Acc#state.keyfun,
+            KeyFunName = atom_value(hd(attribute_arguments(Form))),
+            {false, Acc#state{ keyfun = KeyFunName }};
+        meru_mergefun ->
+            undefined = Acc#state.mergefun,
+            MergeFunName = atom_value(hd(attribute_arguments(Form))),
+            {false, Acc#state{ mergefun = MergeFunName }};
         meru_record ->
             undefined = Acc#state.record,
-            RecordName = hd([atom_value(V) ||
-                V <- list_elements(hd(attribute_arguments(Form)))]),
-            {false, Acc#state{record = RecordName}};
+            RecordName = atom_value(hd(attribute_arguments(Form))),
+            {false, Acc#state{ record = RecordName }};
         _ ->
             {false, Acc}
     end;
 inspect(_, _, _, Acc) ->
     {false, Acc}.
 
-add_get_function(Forms, #state{ record = RecordName }, Context) ->
-    Form = {function, 1, get, 1,
-            [{clause, 1, 
-                [{var, 1, 'Key'}],
+add_funs(Forms, State, Context) ->
+    lists:foldl(fun (FunName, Acc) ->
+        add_fun(FunName, Acc, State, Context)
+    end, Forms, [get]).
+
+add_fun(get, Forms, #state{ record = _RecordName, bucket = BucketName, keyfun = KeyFunName }, Context) ->
+    Form = [{function, 1, get, 1, [{clause,1,
+                [{var,1,'Key'}],
                 [],
-                [{tuple, 1, [{atom, 1, RecordName},
-                             {var, 1, 'Key'}]}]}]},
-    parse_trans:do_insert_forms(above, [Form], Forms, Context).
+                [{call,1,
+                    {remote,1,{atom,1,meru_riak},{atom,1,get}},
+                        [BucketName, {call,1,{atom,1,KeyFunName},[{var,1,'Key'}]}]}]}]}],
+    parse_trans:do_insert_forms(above, Form, Forms, Context).
+% add_fun(put, Forms, #state{ record = RecordName, bucket = BucketName, keyfun = KeyFunName, mergefun = MergeFunName }, Context) ->
+%     Forms;
+% add_fun(put_merge_2, Forms, #state{ record = RecordName, bucket = BucketName, keyfun = KeyFunName, mergefun = MergeFunName }, Context) ->
+%     Forms;
+% add_fun(put_merge_3, Forms, #state{ record = RecordName, bucket = BucketName, keyfun = KeyFunName, mergefun = MergeFunName }, Context) ->
+%     Forms;
+% add_fun(delete, Forms, #state{ record = RecordName, bucket = BucketName, keyfun = KeyFunName, mergefun = MergeFunName }, Context) ->
+%     Forms.
 
 export_funs(Forms, _State, Context) ->
-    Get = [{attribute, 1, export, [{get, 1}]}],
-    parse_trans:do_insert_forms(above, Get, Forms, Context).
+    % Exports = [{attribute, 1, export, [{get, 1}, {put, 1}, {put_merge, 2}, {put_merge, 3}, {delete, 1}]}],
+    Exports = [{attribute, 1, export, [{get, 1}]}],
+    parse_trans:do_insert_forms(above, Exports, Forms, Context).
+
+% binary_value(BinaryTree) ->
+%     list_to_binary([integer_value(binary_field_body(B)) || B <- binary_fields(BinaryTree)]).
+
